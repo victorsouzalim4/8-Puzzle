d089703d51ea0eb1204cf427870b6b4d25e75bd8
Victor <victorsouzalima2064@gmail.com>
2025-05-01 19:40:07 -0300

    class State implemented with the methods fromMatrixString and getNeighbors

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("teste")
+import numpy as np
+from State import State
+
+
+matriz = np.array([
+    [1, 2, 3],
+    [0, 4, 6],
+    [7, 8, 1]
+])
+
+st = State(matriz)
+
+nb = st.getNeighbors()
+
+for n in nb:
+    n.printState()
